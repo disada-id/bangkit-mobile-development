a11a93ffc12873b0cbed682c36fc92a4b0991555
panttech <panttech69@gmail.com>
2023-12-04 22:14:41 +0700

    add Login

--- app/src/main/java/com/example/disadaapp/ui/screen/login/LoginScreen.kt
+++ app/src/main/java/com/example/disadaapp/ui/screen/login/LoginScreen.kt
@@ -1,4 +1,180 @@
 package com.example.disadaapp.ui.screen.login
 
-class LoginScreen {
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.disadaapp.ui.theme.red
+import com.example.disadaapp.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginScreen(
+    modifier: Modifier = Modifier,
+
+) {
+    val email = remember { mutableStateOf("") }
+    val message = remember { mutableStateOf("")}
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+        ) {
+            //display logo
+            Image(
+                painter = painterResource(id = R.drawable.logo) ,
+                contentDescription = "Disada logo",
+//                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(200.dp)
+                    .padding(16.dp)
+            )
+            
+            //display heading
+            Text(
+                text = "Welcome Back !",
+                style = TextStyle(
+                    fontSize = 20.sp,
+//                    fontFamily = FontFamily(Font(R.font.poppins)),
+                    fontWeight = FontWeight(400),
+                    color = Color(0xFF000000),
+                )
+            )
+
+            //display paragraf
+            Text(
+                text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+                style = TextStyle(
+                    fontSize = 12.sp,
+//                    fontFamily = FontFamily(Font(R.font.poppins)),
+                    fontWeight = FontWeight(400),
+                    color = Color(0xFF000000),
+                )
+            )
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+            ) {
+                Text(
+                    text = "Email",
+                    style = TextStyle(
+                        fontSize = 12.sp,
+//                        fontFamily = FontFamily(Font(R.font.poppins)),
+                        fontWeight = FontWeight(400),
+                        color = Color(0xFF000000),
+                    )
+                )
+                OutlinedTextField(
+                    value = "" ,
+                    onValueChange ={},
+                    label = { Text("Email") },
+                    leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "icon" ) },
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        imeAction = ImeAction.Next
+                    ),
+//                keyboardActions = KeyboardActions(
+//                    onNext = { focusState?.moveFocus(FocusDirection.Down) }
+//                ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+
+                Text(
+                    text = "Password",
+                    style = TextStyle(
+                        fontSize = 12.sp,
+//                        fontFamily = FontFamily(Font(R.font.poppins)),
+                        fontWeight = FontWeight(400),
+                        color = Color(0xFF000000),
+                    )
+                )
+
+                // Password input field
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = { /* TODO: Handle password input */ },
+                    label = { Text("Password") },
+                    leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "password" ) },
+                    visualTransformation = PasswordVisualTransformation(),
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        imeAction = ImeAction.Done
+                    ),
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Button(
+                    onClick = { /* TODO: Handle login button click */ },
+                    colors = ButtonDefaults.buttonColors(red),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 16.dp)
+                ) {
+                    Text("Login")
+                }
+            }
+
+
+        }
+
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview(){
+    MaterialTheme {
+        LoginScreen()
+    }
 }
